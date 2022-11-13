b33f80c2a6d632ac85a62669e3bb48dd197e4007
Евгений Глущенко <dartmol21@mail.ru>
2022-11-13 13:08:41 +0300

    add redis

--- venv/Include/main.py
+++ venv/Include/main.py
@@ -1,19 +1,20 @@
 from flask import Flask, jsonify
+import redis
+from rq import Queue
 from flask_cors import CORS
 from weather_test import get_weather
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+r = redis.Redis()
+q = Queue(connection=r)
+
 
 CORS(app)
 
 
 json_weather = get_weather()
-@app.route('/')
-def index():
-    return "Hello, world!"
-
 
 @app.route('/weather/api/v1.0/Murmansk', methods = ['GET'])
 def get_weather():
